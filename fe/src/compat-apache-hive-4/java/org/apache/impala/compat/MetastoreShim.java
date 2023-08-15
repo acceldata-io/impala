bc21731af19d0258fd57bd560bd558a33696e137
manishsinghmowall <manishsingh@acceldata.io>
2023-08-16 00:55:33 +0200

    ODP-560: Impala-Auto invalidate tables issue-CommitTxnEvent is not supported

--- fe/src/compat-apache-hive-4/java/org/apache/impala/compat/MetastoreShim.java
+++ fe/src/compat-apache-hive-4/java/org/apache/impala/compat/MetastoreShim.java
@@ -606,17 +606,16 @@ public class MetastoreShim extends Hive4MetastoreShimBase {
         public CommitTxnEvent(CatalogOpExecutor catalogOpExecutor, Metrics metrics,
                               NotificationEvent event) {
             super(catalogOpExecutor, metrics, event);
-            throw new UnsupportedOperationException("CommitTxnEvent is not supported.");
         }
 
         @Override
         protected void process() throws MetastoreNotificationException {
-
+		throw new MetastoreNotificationException("CommitTxnEvent is not supported.");
         }
 
         @Override
         protected boolean isEventProcessingDisabled() {
-            return false;
+            return true;
         }
 
         @Override
@@ -626,7 +625,7 @@ public class MetastoreShim extends Hive4MetastoreShimBase {
 
         @Override
         protected boolean shouldSkipWhenSyncingToLatestEventId() {
-            return false;
+            return true;
         }
     }
 
