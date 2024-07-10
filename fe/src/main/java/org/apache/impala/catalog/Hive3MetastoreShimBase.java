c2affaa694c24cacf7ca4e0855047066cf46707d
basapuram-kumar <basapuram@acceldata.io>
2024-07-10 22:28:46 +0530

    ODP-1642: Impala-4.4.0 & Hive-4.0.0 compatability support (#7)

--- fe/src/main/java/org/apache/impala/catalog/Hive3MetastoreShimBase.java
+++ fe/src/main/java/org/apache/impala/catalog/Hive3MetastoreShimBase.java
@@ -162,10 +162,11 @@ public class Hive3MetastoreShimBase {
 
   /**
    * Wrapper around MetaStoreUtils.validateName() to deal with added arguments.
-   */
+
   public static boolean validateName(String name) {
-    return MetaStoreUtils.validateName(name, null);
+    return MetaStoreServerUtils.validateName(name, null);
   }
+*/
 
   /**
    * Wrapper around IMetaStoreClient.alter_partition() to deal with added arguments.
@@ -830,12 +831,5 @@ public class Hive3MetastoreShimBase {
     // TODO(IMPALA-9088): deal with customized transformer in HMS.
     return wh.getDefaultTablePath(db, tbl.getTableName().toLowerCase(), isExternal)
         .toString();
-  }
-
-  /**
-   * At the Metadata level there are no restrictions on column names.
-   */
-  public static boolean validateColumnName(String name) {
-    return MetaStoreUtils.validateColumnName(name);
   }
 }
