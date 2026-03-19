92e4b608c99fec9b3a7f455391159bcb30585c54
kravii <ravi@acceldata.io>
2026-03-19 16:15:08 -0400

    OSV-12818 Updated ambari-python wrap

--- shell/ext-py/sqlparse-0.5.1/setup.py
+++ shell/ext-py/sqlparse-0.5.1/setup.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env ambari-python-wrap
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009-2018 the sqlparse authors and contributors
+# <see AUTHORS file>
+#
+# This setup script is part of python-sqlparse and is released under
+# the BSD License: https://opensource.org/licenses/BSD-3-Clause
+
 import re
   from setuptools import setup, find_packages
 
